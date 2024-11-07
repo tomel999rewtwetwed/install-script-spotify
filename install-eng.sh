2a36f4da587f58a953328712b47f0fb368ff0330
tomel999 <146027763+tomel999rewtwetwed@users.noreply.github.com>
2024-11-07 20:48:53 +0100

    Create install-eng.sh

---
+++ install-eng.sh
@@ -0,0 +1,10 @@
+
+
+sudo su
+apt update
+apt apt install -y git nodejs npm python3 pip
+git clone https://github.com/tomel999rewtwetwed/spotify-web.git
+cd spotify-web
+npm install
+npm install express
+npm install discord-rcp
