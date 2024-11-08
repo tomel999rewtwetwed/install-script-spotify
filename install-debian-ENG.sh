4b628f35d267d5aabfda261654da50bc602311dc
tomel999 <146027763+tomel999rewtwetwed@users.noreply.github.com>
2024-11-08 19:19:33 +0100

    Create install-debian-ENG.sh

---
+++ install-debian-ENG.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y git python3 python3-pip curl
+
+curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
+sudo apt install -y nodejs
+
+git clone https://github.com/tomel999rewtwetwed/spotify-web.git
+
+cd spotify-web || exit 1
+
+npm install
+
+npm install express discord-rcp
+
+pip install spotdl --break-system-packages
